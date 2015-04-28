f7264516c7a5a0b182814f75aaa42938fb99d098
Jim Patt & Cody Bouscaren <student@epicodus.com>
2015-04-28 14:04:10 -0700

    Passes initial spec

--- spec/tama_spec.rb
+++ spec/tama_spec.rb
@@ -1,10 +1,16 @@
-describe(Tamagotchi) do
+require('rspec')
+require('tama')
+require('pry')
+
+
+describe(Tama) do
   describe("#initialize") do
     it("sets the name and life levels of a new Tamagotchi") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tama.new("lil dragon")
       expect(my_pet.name()).to(eq("lil dragon"))
       expect(my_pet.food_level()).to(eq(10))
       expect(my_pet.sleep_level()).to(eq(10))
       expect(my_pet.activity_level()).to(eq(10))
     end
+  end
   end
